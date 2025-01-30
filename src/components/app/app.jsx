00816aec9ee358f9599132b9ffbdadb4d2e9ba9b
TurobSulaymonov <turobsulaymonov117@gmail.com>
2025-01-30 12:58:55 +0900

    fix: transfer lazy loading from simple loading

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,28 @@
+import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import DetailedPage from '../../pages/detailed-page'
-import HomePage from '../../pages/home-page'
-import NotFoundPage from '../../pages/not-found-page'
 import Navbar from '../navbar/navbar'
-import TrandingPage from '../../pages/tranding-page'
-import PopularPage from '../../pages/popular-page'
+import Spinner from '../spinner/spinner'
+
+const NotFoundPage = lazy(() => import("../../pages/home-page"));
+const HomePage = lazy(() => import("../../pages/home-page"));
+const TrandingPage = lazy(() => import('../../pages/tranding-page'));
+const PopularPage = lazy(() => import('../../pages/tranding-page'))
+const DetailedPage = lazy(() => import('../../pages/detailed-page'))
 
 const App = () => {
 	return (
 		<div className='app'>
 		<Navbar />
-		<Routes>
+		<Suspense fallback={<Spinner/>}>
+		 <Routes>
 			<Route path='/' element={<HomePage />} />
 			<Route path='/tranding' element={<TrandingPage/>} />
 			<Route path='/popular' element={<PopularPage/>} />
 			<Route path='/movie/:movieId' element={<DetailedPage />} />
 			<Route path='*' element={<NotFoundPage />} />
-		</Routes>
+		 </Routes>
+		</Suspense>
+	
 	</div>
 	)
 }
